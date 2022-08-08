8b7db1737e01948f400d74fe206699a3dc06760e
deywid <deywid.felipe@gmail.com>
2022-08-07 21:40:50 -0300

    Refactor: changed store style

---
+++ react/SliderComponent.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { Spinner } from "vtex.styleguide";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./styles.module.css";
+
+export default function SliderComponent() {
+  const [combos, setCombos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = axios.get(
+          `https://fyzduqlrdrwd7xcsan75qflehy0anpmj.lambda-url.us-east-1.on.aws/`
+          //`https://combos-api.herokuapp.com/combos/available`
+        );
+        await response.then(({ data }) => {
+          console.log("data", data);
+          setCombos(data);
+        });
+        setLoading(false);
+      } catch (error) {
+        console.error(`erro aqui ${error}`);
+      }
+    };
+
+    getData();
+  }, []);
+
+  loading && (
+    <div className={styles.spinnerContainer}>
+      <Spinner />
+    </div>
+  );
+
+  if (!combos || !combos.length) return null;
+
+  const handleChevronRightClick = () => {
+    console.log(sliderRef.current.offsetWidth);
+    sliderRef.current.scrollLeft += sliderRef.current.offsetWidth;
+  };
+
+  const handleChevronLeftClick = () => {
+    console.log(sliderRef.current.offsetWidth);
+    sliderRef.current.scrollLeft -= sliderRef.current.offsetWidth;
+  };
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Chevron} onClick={handleChevronLeftClick}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </div>
+      <div className={styles.Slider} ref={sliderRef}>
+        {combos.map((comboItem) => {
+          return (
+            <div key={comboItem.id} className={styles.SliderItem}>
+              <div>
+                <img
+                  src={comboItem.products[0].skus[0].image}
+                  alt={comboItem.alt}
+                />
+                <h2 className={styles.SliderItemTitle}>{comboItem.products[0].name}</h2>
+                <h3 className={styles.SliderItemPrice}>{comboItem.products[0].skus[0].listPriceFormated}</h3>
+              </div>
+              <div className={styles.PlusContainer}>
+                <FontAwesomeIcon icon={faPlus} />
+              </div>
+              <div>
+                <img src={comboItem.products[1].skus[0].image} alt={comboItem.alt} />
+                <h2 className={styles.SliderItemTitle}>{comboItem.products[1].name}</h2>
+                <h3 className={styles.SliderItemPrice}>{comboItem.products[1].skus[0].listPriceFormated}</h3>
+              </div>
+              <div className={styles.SliderButtonContainer}>
+                <button className={styles.SliderButton}>
+                  Adicionar ao carrinho
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.Chevron} onClick={handleChevronRightClick}>
+        <FontAwesomeIcon icon={faChevronRight} />
+      </div>
+    </div>
+  );
+}
